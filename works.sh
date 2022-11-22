f04cadf7b468a63fba8b7c715b57e2006fb83e42
dan.castillo <dancastillo99@gmail.com>
2022-11-21 22:35:50 -0600

    testing a change right here

--- works.sh
+++ works.sh
@@ -1,4 +1,2 @@
-LASTMERGES=$(git log --pretty=format:"%h%ad%x09%an%x09%s" --date=short main...develop)
+DIFF_MERGES=$(git log --pretty=format:"%h%ad%x09%an%x09%s" --date=short main...develop)
 echo "$LASTMERGES"
-# echo "LASTMERGES<<EOF" >> $GITHUB_ENV
-# echo $GITHUB_ENV
