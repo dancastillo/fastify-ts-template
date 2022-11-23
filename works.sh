83c05e2b1a2ae2cebc67754bd731cc47815eca10
dan.castillo <dancastillo99@gmail.com>
2022-11-22 20:14:00 -0600

    new commit here

--- works.sh
+++ works.sh
@@ -1,2 +1,8 @@
 DIFF_MERGES=$(git log --pretty=format:"%h%ad%x09%an%x09%s" --date=short main...develop)
-echo "$LASTMERGES"
+IFS=', ' read -r -a array <<< "$DIFF_MERGES"
+# echo "$LASTMERGES"
+
+for element in "${array[@]}"
+do
+    echo "$element"
+done
